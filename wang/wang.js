88f6aaccdf7d54c4b7ea22c481630eb060ed8f6b
swettee <1419871146@qq.com>
2025-02-28 21:40:04 +0800

    Update wang.js

--- wang/wang.js
+++ wang/wang.js
@@ -1,15 +1,19 @@
-function startGame() {
+// 获取按钮元素
+const startBtn = document.getElementById("startBtn");
+
+// 为按钮添加点击事件监听器
+startBtn.addEventListener("click", function () {
     // 模拟游戏开始
     alert("游戏开始啦！");
 
     // 模拟击败小兵进度增加
-    let progressBar = document.getElementById("progress-bar");
+    const progressBar = document.getElementById("progress-bar");
     let currentValue = parseInt(progressBar.value);
     let maxValue = parseInt(progressBar.max);
-    let scoreElement = document.querySelector(".score p");
+    const scoreElement = document.querySelector(".score p");
     let score = parseInt(scoreElement.textContent.split(": ")[1]);
 
-    let intervalId = setInterval(() => {
+    const intervalId = setInterval(() => {
         currentValue++;
         progressBar.value = currentValue;
 
@@ -19,4 +23,4 @@ function startGame() {
             scoreElement.textContent = `当前得分：${score}`;
         }
     }, 100);
-}
+});
